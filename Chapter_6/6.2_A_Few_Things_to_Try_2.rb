f26ac454c1ebf6b9245d1a7adf4aba13e0bff6a4
Alex Henegar <alex.henegar19@gmail.com>
2017-02-13 04:21:49 -0500

    Upload Things to Try 2 again

--- Chapter_6/6.2_A_Few_Things_to_Try_2.rb
+++ Chapter_6/6.2_A_Few_Things_to_Try_2.rb
@@ -1,16 +1,7 @@
-l_line_width = 20
-r_line_width = 20
-ch_line_width = 13
-total_line_width = ch_line_width + l_line_width + r_line_width
-
-ch_1_title = "Getting Started"
-ch_1_pg = "1"
-ch_2_title = "Numbers"
-ch_2_pg = "9"
-ch_3_title = "Letters"
-ch_3_pg = "13"
+line_width = 30
+total_line_width = 40
 
 puts 'Table of Contents'.center(total_line_width)
-puts 'Chapter 1:  '.ljust(ch_line_width) + ch_1_title.ljust(l_line_width) + ('page ' + ch_1_pg).ljust(r_line_width)
-puts 'Chapter 2:  '.ljust(ch_line_width) + ch_2_title.ljust(l_line_width) + ('page ' + ch_2_pg).ljust(r_line_width)
-puts 'Chapter 3:  '.ljust(ch_line_width) + ch_3_title.ljust(l_line_width) + ('page ' + ch_3_pg).ljust(r_line_width)
+puts 'Chapter 1:  Getting Started'.ljust(line_width) + 'page 1'
+puts 'Chapter 1:  Numbers'.ljust(line_width) + 'page 9'
+puts 'Chapter 1:  Letters'.ljust(line_width) + 'page 13'
