fc1c373a812c7b7e2733768d8363bfa2db0247ef
Alex Henegar <alex.henegar19@gmail.com>
2017-02-17 15:27:09 -0500

    Update file names to Book Solution files.

--- Chapter_11/A_Few_Things_to_Try_3.rb
+++ Chapter_11/A_Few_Things_to_Try_3.rb
@@ -4,66 +4,53 @@
   A purely random shuffle just doesn't quite seem...mixed up enough.
   (Random and mixed up are not at all the same thign. Random is total clumpy.)
 
+
 =end
 
 
-# shuffle the list
-def music_shuffle filenames
-  # We don't want a perfectly random shuffle, so let's
-  # instead do a shuffle like card-shuffling. Let's
-  # shuffle the "deck" twice, then cut it once. That's
-  # not enough times to make a perfect shuffle, but it
-  # does mix things up a bit.
-  # Before we do anything, let's actually *sort* the
-  # input, since we don't know how shuffled it might
-  # already be, and we don't want it to be *too* random.
+# The shuffle methods to randomize a list
+def shuffle list
 
-  filenames = filenames.sort
-  len       = filenames.length
+  shuffle_rec list, []
+end
+def shuffle_rec remaining, shuffled
+  # if all have been shuffled, return the shuffled list
+  if remaining.length <= 0
+    return shuffled
+  end
+  # how many are left to shuffle?
+  left = remaining.length
+  # choose a random index to be the next item index
+  next_item_idx = rand(left)
+  new_arr = []
 
-  # Now we shuffle five times
-  5.times do
-    l_idx = 0         # index of next card in left pile
-    r_idx = len / 2   # index of next card in right pile
-    shuf = []
-    # NOTE: If we have an odd number of "cards",
-    #       then the right rill will be larger.
-
-    while shuf.length < len
-      if shuf.length % 2 == 0
-        # take card from right pile
-        shuf.push(filenames[r_idx])
-        r_idx = r_idx + 1
-      else
-        # take card from left pile
-        shuf.push(filenames[l_idx])
-        l_idx = l_idx + 1
-      end
+  # if the item index in remaining matches the random one, then add it
+  # to the shuffled list
+  # if not, add it to the new_arr which will become the new remaining list
+  current_idx = 0
+  remaining.each do |item|
+    if current_idx == next_item_idx
+      shuffled.push item
+    else
+      new_arr.push item
     end
-    filenames = shuf
+    current_idx += 1
   end
-
-  # And cut the deck.
-  arr = []
-  cut = rand(len) # index of card to cut at
-  idx = 0
-
-  while idx < len
-    arr.push(filenames[(idx+cut)%len])
-    idx = idx + 1
-  end
-  arr
+  shuffle_rec new_arr, shuffled
 end
 
 # This is where the playlist will go.
 Dir.chdir '/media/ajdot/Barracuda/Users/Alex/Programming-Local/GitHub Repositories/AJDot/Learn_to_Program_The_Facets_of_Ruby/git_ignore/MusicPlaylist'
 
 # Look for music here
-music_names = Dir['/media/ajdot/Barracuda/Users/Alex/Music/iTunes/iTunes Media/Music/{Aqualung,Guster,Angels & Airwaves,Ben Folds}/**/*.m4a']
+music_names = Dir['/media/ajdot/Barracuda/Users/Alex/Music/iTunes/iTunes Media/Music/{Aqualung,Guster,Angels & Airwaves}/**/*.m4a']
+
+# shuffle the list
+music_names_shuffled = shuffle music_names
 
 filename = 'new_playlist_better.m3u'
 File.open filename, 'w' do |f|
-  music_shuffle(music_names).each do |name|
+  music_names_shuffled.each do |name|
     f.write name + "\n"
   end
 end
---
+++ Chapter_11/A_Few_Things_to_Try_3_BookSol_2.rb
@@ -0,0 +1,71 @@
+=begin
+  Build a better playlist.
+
+  A purely random shuffle just doesn't quite seem...mixed up enough.
+  (Random and mixed up are not at all the same thign. Random is total clumpy.)
+
+=end
+
+
+# shuffle the list
+def music_shuffle filenames
+  # We don't want a perfectly random shuffle, so let's
+  # instead do a shuffle like card-shuffling. Let's
+  # shuffle the "deck" twice, then cut it once. That's
+  # not enough times to make a perfect shuffle, but it
+  # does mix things up a bit.
+  # Before we do anything, let's actually *sort* the
+  # input, since we don't know how shuffled it might
+  # already be, and we don't want it to be *too* random.
+
+  filenames = filenames.sort
+  len       = filenames.length
+
+  # Now we shuffle five times
+  5.times do
+    l_idx = 0         # index of next card in left pile
+    r_idx = len / 2   # index of next card in right pile
+    shuf = []
+    # NOTE: If we have an odd number of "cards",
+    #       then the right rill will be larger.
+
+    while shuf.length < len
+      if shuf.length % 2 == 0
+        # take card from right pile
+        shuf.push(filenames[r_idx])
+        r_idx = r_idx + 1
+      else
+        # take card from left pile
+        shuf.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
+    end
+    filenames = shuf
+  end
+
+  # And cut the deck.
+  arr = []
+  cut = rand(len) # index of card to cut at
+  idx = 0
+
+  while idx < len
+    arr.push(filenames[(idx+cut)%len])
+    idx = idx + 1
+  end
+  arr
+end
+
+# This is where the playlist will go.
+Dir.chdir '/media/ajdot/Barracuda/Users/Alex/Programming-Local/GitHub Repositories/AJDot/Learn_to_Program_The_Facets_of_Ruby/git_ignore/MusicPlaylist'
+
+# Look for music here
+music_names = Dir['/media/ajdot/Barracuda/Users/Alex/Music/iTunes/iTunes Media/Music/{Aqualung,Guster,Angels & Airwaves,Ben Folds}/**/*.m4a']
+
+filename = 'new_playlist_better.m3u'
+File.open filename, 'w' do |f|
+  music_shuffle(music_names).each do |name|
+    f.write name + "\n"
+  end
+end
+
+puts 'Done!'
