c536ca769e8ab4bf9e605a69ba665f29575b3d12
Alex Henegar <alex.henegar19@gmail.com>
2017-02-17 21:40:05 -0500

    edit BookSol_2 - incomplete program

--- Chapter_11/A_Few_Things_to_Try_3_BookSol_2.rb
+++ Chapter_11/A_Few_Things_to_Try_3_BookSol_2.rb
@@ -1,3 +1,7 @@
+########################
+# INCOMPLETE
+########################
+
 =begin
   Build a better playlist.
 
@@ -6,53 +10,29 @@
 
 =end
 
+=begin
 
-# shuffle the list
+  A better way would be mix more carefully and on every level (artist, album).
+
+  Find similar song (like on the same CD), mix them up, and spread them out as
+  far away from each other as I can in teh next grouping (like songs by the
+  same artist). Then do the same for teh next level up (like genre if it exists)
+
+=end
+
 def music_shuffle filenames
-  # We don't want a perfectly random shuffle, so let's
-  # instead do a shuffle like card-shuffling. Let's
-  # shuffle the "deck" twice, then cut it once. That's
-  # not enough times to make a perfect shuffle, but it
-  # does mix things up a bit.
-  # Before we do anything, let's actually *sort* the
-  # input, since we don't know how shuffled it might
-  # already be, and we don't want it to be *too* random.
-
-  filenames = filenames.sort
-  len       = filenames.length
-
-  # Now we shuffle five times
-  5.times do
-    l_idx = 0         # index of next card in left pile
-    r_idx = len / 2   # index of next card in right pile
-    shuf = []
-    # NOTE: If we have an odd number of "cards",
-    #       then the right rill will be larger.
-
-    while shuf.length < len
-      if shuf.length % 2 == 0
-        # take card from right pile
-        shuf.push(filenames[r_idx])
-        r_idx = r_idx + 1
-      else
-        # take card from left pile
-        shuf.push(filenames[l_idx])
-        l_idx = l_idx + 1
-      end
-    end
-    filenames = shuf
+  songs_and_paths = filenames.map do |s|
+    [s, s.split('/')]   # [song, path]
   end
 
-  # And cut the deck.
-  arr = []
-  cut = rand(len) # index of card to cut at
-  idx = 0
+  tree = {:root => []}
 
-  while idx < len
-    arr.push(filenames[(idx+cut)%len])
-    idx = idx + 1
-  end
-  arr
+  # put each song into the tree
+  insert_into_tree
+
+  # ##################
+  # NEED TO LEARN PROC
+  # ##################
 end
 
 # This is where the playlist will go.
