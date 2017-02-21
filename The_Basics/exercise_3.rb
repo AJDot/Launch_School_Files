f3ac65757c08c460b4ffe1df65814699197ff87d
Alex Henegar <alex.henegar19@gmail.com>
2017-02-21 11:54:16 -0500

    Add sols to exerices in 'The Basics'

---
+++ The_Basics/exercise_3.rb
@@ -0,0 +1,7 @@
+movies = {:Gladiator                        => 2000,
+          :Braveheart                       => 1995,
+          :How_the_Grinch_Stole_Christmas!  => 1966}
+
+puts movies[:Gladiator]
+puts movies[:Braveheart]
+puts movies[:How_the_Grinch_Stole_Christmas!]
