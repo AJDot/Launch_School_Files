f6d73b589741df5f2fa5c83a0b3e57724d0ff9f2
Alex Henegar <alex.henegar19@gmail.com>
2017-02-22 11:11:38 -0500

    Add exercises from Variables chapter

---
+++ Variables/exercise_5.rb
@@ -0,0 +1,2 @@
+puts "First case: print 3 to the screen"
+puts "Second case: error - x is defined in the scope of times and is not accessible outside of the times method"
---
+++ Variables/name.rb
@@ -0,0 +1,7 @@
+puts 'Put your first name here:'
+first_name = gets.chomp
+puts 'Put your last name here:'
+last_name = gets.chomp
+
+
+puts 'Welcome ' + first_name + ' ' + last_name + '!'
