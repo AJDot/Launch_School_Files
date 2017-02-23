75e2b0dc93ba44ec11fa1d618ba5ec887a942dbc
Alex Henegar <alex.henegar19@gmail.com>
2017-02-23 11:44:07 -0500

    Add exercises for 'Array' chapter

---
+++ Arrays/exercise_1.rb
@@ -0,0 +1,8 @@
+# Write a program that check to see if the number
+# appears in the array
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+puts "Is #{number} in the array #{arr}?"
+puts "Answer: #{arr.include?(number)}"
