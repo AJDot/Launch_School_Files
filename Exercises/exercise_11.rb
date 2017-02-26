2465fbc0d8e6c22e56b262856039a2445249cc01
AJDot <alex.henegar19@gmail.com>
2017-02-26 18:19:26 -0500

    add 'Exercises' files

---
+++ Exercises/exercise_11.rb
@@ -0,0 +1,7 @@
+# Look at several Rails/Ruby online API sources and say which one you like best
+# and why.
+
+# ANSWER
+# Simply looking at ruby-doc.org seems like the best source of Ruby
+# documentation. Everything is laid out in an organize way, navigation is easy.
+# The examples I have read are clear and demonstrate the concept well.
---
+++ Exercises/exercise_7.rb
@@ -0,0 +1,5 @@
+# What's the major difference between an Array and a Hash?
+
+# ANSWER
+# Arrays are ordered lists that are indexed using and integer.
+# Hashes are unordered lists that are indexed using a key-value pair.
