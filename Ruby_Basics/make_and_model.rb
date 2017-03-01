ae4cc2eb019b6f64c205b3aee3f216d3b104cbc2
Alex Henegar <alex.henegar19@gmail.com>
2017-03-01 16:32:08 -0500

    Add initial Ruby Basics Methods files

---
+++ Ruby_Basics/make_and_model.rb
@@ -0,0 +1,18 @@
+# Give us your feedback
+# Make and Model
+#
+# Using the following code, write a method called car that takes two arguments
+#  and prints a string containing the values of both arguments.
+#
+# car('Toyota', 'Corolla')
+# Expected output:
+#
+# Toyota Corolla
+
+# ANSWER
+
+def car(make, model)
+  "#{make} #{model}"
+end
+
+puts car('Toyota', 'Corolla')
