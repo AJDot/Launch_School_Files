50eea5540932108f46f83847cffd1f9af17ca277
AJDot <alex.henegar19@gmail.com>
2017-03-02 10:40:02 -0500

    upload Ruby_Basics/Hashes files

---
+++ Ruby_Basics/Hashes/which_collection.rb
@@ -0,0 +1,16 @@
+# Give us your feedback
+# Which Collection?
+#
+# Rewrite car as a nested array containing the same key-value pairs.
+#
+# car = {
+#   type:  'sedan',
+#   color: 'blue',
+#   year:  2003
+# }
+
+# ANSWER
+car = [
+  [:type,  'sedan'],
+  [:color, 'blue'],
+  [:year,  2003]]
