1aed11a30c0d9c22fc7f2b3f45f760a9e5db3af9
AJDot <alex.henegar19@gmail.com>
2017-03-01 22:37:35 -0500

    Add Ruby_Basics\Return\ exercises

---
+++ Ruby_Basics/Return/breakfast_lunch_dinner_part_6.rb
@@ -0,0 +1,15 @@
+# Breakfast, Lunch, or Dinner? (Part 6)
+#
+# What will the following code print? Why? Don't run it until you've attempted
+# to answer.
+#
+# def meal
+#   return 'Breakfast'
+#   'Dinner'
+#   puts 'Dinner'
+# end
+#
+# puts meal
+
+# ANSWER
+# Breakfast
---
+++ Ruby_Basics/Return/tricky_number.rb
@@ -0,0 +1,17 @@
+# Tricky Number
+#
+# What will the following code print? Why? Don't run it until you've attempted
+# to answer.
+#
+# def tricky_number
+#   if true
+#     number = 1
+#   else
+#     2
+#   end
+# end
+#
+# puts tricky_number
+
+# ANSWER
+# 1
