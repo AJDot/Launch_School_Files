bd9e9bccc5dac9522161adacfeabb0074f4d37e6
Alex Henegar <alex.henegar19@gmail.com>
2017-03-10 21:40:00 -0500

    calculator.rb after rubocop

--- 101_Programming_Foundations/Lesson_2/calculator.rb
+++ 101_Programming_Foundations/Lesson_2/calculator.rb
@@ -40,12 +40,9 @@ def operation_to_message(op)
   end
 end
 
-
 prompt("Hi #{name}!")
 
 loop do # main loop
-
-
   number1 = ''
   loop do
     prompt("What's the first number?")
@@ -92,16 +89,16 @@ loop do # main loop
 
   prompt("#{operation_to_message(operator)} the two numbers...")
 
-  result = case operator
-           when '1'
-             number1.to_i + number2.to_i
-           when '2'
-             number1.to_i - number2.to_i
-           when '3'
-             number1.to_i * number2.to_i
-           when '4'
-               number1.to_f / number2.to_f
-  end
+  result =  case operator
+            when '1'
+              number1.to_i + number2.to_i
+            when '2'
+              number1.to_i - number2.to_i
+            when '3'
+              number1.to_i * number2.to_i
+            when '4'
+              number1.to_f / number2.to_f
+            end
 
   prompt("The result is #{result}")
 
