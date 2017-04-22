2fa66d64f18c2071682c60123ae3cb77fa26ab4d
Alex Henegar <alex.henegar19@gmail.com>
2017-04-22 20:54:30 +0000

    implement the choose method

--- 120_Object_Oriented_Programming/lesson_2/RPS.rb
+++ 120_Object_Oriented_Programming/lesson_2/RPS.rb
@@ -21,12 +21,30 @@
 
 # Step 4
 class Player
-  def initialize
-    # maybe a "name"? what about a "move"?
+  attr_accessor :move
+
+  def initialize(player_type = :human)
+    @player_type = player_type
+    @move = nil
   end
 
   def choose
+    if human?
+      choice = nil
+      loop do
+        puts "Please choose rock, paper, or scissors:"
+        choice = gets.chomp
+        break if ['rock', 'paper', 'scissors'].include? choice
+        puts "sorry, invalid choice."
+      end
+      self.move = choice
+    else
+      self.move = ['rock', 'paper', 'scissors'].sample
+    end
+  end
 
+  def human?
+    @player_type == :human
   end
 end
 
@@ -47,3 +65,33 @@ end
 def compare(move1, move2)
 
 end
+
+# After the noun/verb organization, we need an "engine" of some sort to orchestrate the objects.
+# The "Engine" is where the procedural program flow should be.
+
+class RPSGame # The Engine
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Player.new
+    @computer = Player.new(:computer)
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
+  end
+
+  def play
+    display_welcome_message
+    human.choose
+    computer.choose
+    display_winner
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
