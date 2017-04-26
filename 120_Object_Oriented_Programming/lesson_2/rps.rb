a0888e60d8c552398f1c9c3c7df77b684af5cbd6
Alex Henegar <alex.henegar19@gmail.com>
2017-04-26 00:54:33 +0000

    redo history implementation again - it is now a collaborator class to RPS class

--- 120_Object_Oriented_Programming/lesson_2/rps.rb
+++ 120_Object_Oriented_Programming/lesson_2/rps.rb
@@ -1,22 +1,107 @@
 require 'pry'
 
-def prompt(msg)
-  puts "=> #{msg}"
-end
+# Most items displayed to screen
+module Displayable
+  def display_welcome_message
+    prompt '------------------------------------------------'
+    prompt 'Welcome to Rock, Paper, Scissors, Spock, Lizard!'
+    prompt "First to #{self.class::MAX_SCORE} wins!"
+    prompt '------------------------------------------------'
+  end
 
-def join_or(array)
-  case array.size
-  when 1
-    array.first
-  when 2
-    "#{array.first} or #{array.last}"
-  else
-    "#{array[0..-2].join(', ')} or #{array.last}"
+  def display_goodbye_message
+    prompt '------------------------------------------------------------------'
+    prompt 'Thanks for playing Rock, Paper, Scissors, Spock, Lizard. Good bye!'
+    prompt '------------------------------------------------------------------'
+  end
+
+  def display_moves
+    prompt "#{human.name} chose #{human.move}."
+    prompt "#{computer.name} chose #{computer.move}."
+  end
+
+  def display_winner
+    human_move = human.move
+    computer_move = computer.move
+    if human_move > computer_move
+      prompt "#{human.name} won the round!"
+    elsif human_move < computer_move
+      prompt "#{computer.name} won the round!"
+    else
+      prompt "It's a tie!"
+    end
+  end
+
+  def format_history
+    result = []
+    history.human_moves.size.times do |move_num|
+      line = ''
+      line << history.human_moves[move_num].to_s.center(12) + '     '
+      line << history.computer_moves[move_num].to_s.center(12)
+      line << history.game_num[move_num].center(4)
+      line << history.round_num[move_num].center(10)
+      result << line
+    end
+    result
+  end
+
+  def display_history
+    prompt '-----------History----------Game---Round---'
+    format_history.each { |line| prompt line }
+    prompt '-------------------------------------------'
+  end
+
+  def display_game_winner
+    winner = human.score == self.class::MAX_SCORE ? human : computer
+    prompt "#{winner.name} won the game!"
+  end
+
+  def display_score
+    human_score = human.score
+    computer_score = computer.score
+    spacing = ' ' * 16
+
+    prompt '---------Round Score---------'
+    prompt "#{human_score}#{spacing}#{computer_score}".center(29)
+    prompt '---------Game Score----------'
+    prompt "#{human_score.game}#{spacing}#{computer_score.game}".center(29)
+    prompt ''
+  end
+
+  def display_header
+    prompt human.display_in_header + '-----' + computer.display_in_header
+  end
+
+  def show_display
+    clear_screen
+    display_moves
+    display_winner
+    display_header
+    display_score
+    display_history
+  end
+
+  def clear_screen
+    system('clear') || system('cls')
   end
 end
 
-def clear_screen
-  system('clear') || system('cls')
+# Methods to help format basic objects
+module Formatting
+  def prompt(msg)
+    puts "=> #{msg}"
+  end
+
+  def join_or(array)
+    case array.size
+    when 1
+      array.first
+    when 2
+      "#{array.first} or #{array.last}"
+    else
+      "#{array[0..-2].join(', ')} or #{array.last}"
+    end
+  end
 end
 
 class Move
@@ -103,6 +188,7 @@ class Player
 end
 
 class Human < Player
+  include Formatting
   def set_name
     n = ''
     loop do
@@ -147,6 +233,13 @@ class Computer < Player
   end
 end
 
+class WeightedChoice < Computer
+  def choose
+    super
+  end
+
+end
+
 # Collaborator class for Player
 class Score
   attr_reader :round
@@ -175,104 +268,72 @@ class Score
   end
 end
 
-# Most items displayed to screen
-module Displayable
-  def display_welcome_message
-    prompt '------------------------------------------------'
-    prompt 'Welcome to Rock, Paper, Scissors, Spock, Lizard!'
-    prompt "First to #{self.class::MAX_SCORE} wins!"
-    prompt '------------------------------------------------'
+class History
+  attr_reader :human_moves, :computer_moves, :game_num, :round_num
+  attr_accessor :round, :game
+
+  def initialize(human, computer)
+    @human = human
+    @computer = computer
+    @human_moves = []
+    @computer_moves = []
+    @game_num = []
+    @round_num = []
+    @game = 1
+    @round = 0
   end
 
-  def display_goodbye_message
-    prompt '------------------------------------------------------------------'
-    prompt 'Thanks for playing Rock, Paper, Scissors, Spock, Lizard. Good bye!'
-    prompt '------------------------------------------------------------------'
+  def update
+    @human_moves << @human.move
+    @computer_moves << @computer.move
+    @game_num << @game.to_s
+    @round_num << @round.to_s
   end
 
-  def display_moves
-    prompt "#{human.name} chose #{human.move}."
-    prompt "#{computer.name} chose #{computer.move}."
-  end
-
-  def display_winner
-    human_move = human.move
-    computer_move = computer.move
-    if human_move > computer_move
-      prompt "#{human.name} won the round!"
-    elsif human_move < computer_move
-      prompt "#{computer.name} won the round!"
-    else
-      prompt "It's a tie!"
-    end
-  end
-
-  def format_history
-    result = []
-    @history[:human].size.times do |move_num|
-      line = ''
-      # binding.pry
-      line << @history[:human][move_num].center(12) + '     '
-      line << @history[:computer][move_num].center(12)
-      line << @history[:game][move_num].center(4)
-      line << @history[:round][move_num].center(10)
-      result << line
-    end
-    result
-  end
-
-  def display_history
-    prompt '-----------History----------Game---Round---'
-    format_history.each { |line| prompt line }
-    prompt '-------------------------------------------'
-  end
-
-  def display_game_winner
-    winner = human.score == self.class::MAX_SCORE ? human : computer
-    prompt "#{winner.name} won the game!"
-  end
-
-  def display_score
-    human_score = human.score
-    computer_score = computer.score
-    spacing = ' ' * 16
-
-    prompt '---------Round Score---------'
-    prompt "#{human_score}#{spacing}#{computer_score}".center(29)
-    prompt '---------Game Score----------'
-    prompt "#{human_score.game}#{spacing}#{computer_score.game}".center(29)
-    prompt ''
-  end
-
-  def display_header
-    prompt human.display_in_header + '-----' + computer.display_in_header
-  end
-
-  def show_display
-    clear_screen
-    display_moves
-    display_winner
-    display_header
-    display_score
-    display_history
+  def total_rounds
+    @round_num.size
   end
 end
 
+class Rule
+  def initialize(history)
+    @win_counts = { 'rock' => 0, 'paper' => 0, 'scissors' => 0, 'Spock' => 0, 'lizard' => 0 }
+    @win_percents = { 'rock' => 0, 'paper' => 0, 'scissors' => 0, 'Spock' => 0, 'lizard' => 0 }
+    @history = history
+  end
+
+  def update_percents
+    human_move = @history.human_moves.last
+    computer_move = @history.computer_moves.last
+    @win_counts[computer_move.to_s] += 1 if computer_move > human_move
+    update_win_percents
+    p @history.total_rounds
+    p @win_counts
+    p @win_percents
+  end
+
+  def update_win_percents
+    @win_percents.keys.each do |move|
+      @win_percents[move] = @win_counts[move].to_f / @history.total_rounds * 100
+    end
+  end
+end
+
+
 # Game Orchestration Engine
 class RPSGame
-  include Displayable
+  include Displayable, Formatting
 
   MAX_SCORE = 3
 
-  attr_accessor :human, :computer
+  attr_accessor :human, :computer, :history
 
   def initialize
     clear_screen
     @human = Human.new
-    @computer = Computer.new
-    @game = 1
-    @round = 0
-    @history = { human: [], computer: [], game: [], round: [] }
+    @computer = WeightedChoice.new # Computer.new
+    @history = History.new(@human, @computer)
+    @rule = Rule.new(history)
   end
 
   private
@@ -288,22 +349,16 @@ class RPSGame
   def next_game
     human.score.reset
     computer.score.reset
-    @game += 1
-    @round = 0
+    history.game += 1
+    history.round = 0
   end
 
   def update_round
-    @round += 1
+    history.round += 1
   end
 
   def update_history
-    @history[:human] << human.move.to_s
-    @history[:computer] << computer.move.to_s
-  end
-
-  def update_game_round
-    @history[:game] << @game.to_s
-    @history[:round] << @round.to_s
+    history.update
   end
 
   def show_after_game_display
@@ -333,9 +388,9 @@ class RPSGame
     human.choose
     computer.choose
     update_history
-    update_game_round
     update_score
     show_display
+    @rule.update_percents
   end
 
   public
