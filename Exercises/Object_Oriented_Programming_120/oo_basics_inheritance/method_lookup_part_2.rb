abe0cf09471c82bac5373fd13ce3c11025b6f932
Alex Henegar <alex.henegar19@gmail.com>
2017-05-03 09:57:01 -0400

    add oo_basics_inheritance exercises

---
+++ Exercises/Object_Oriented_Programming_120/oo_basics_inheritance/method_lookup_part_2.rb
@@ -0,0 +1,17 @@
+class Animal
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new
+cat1.color
+
+puts 'Cat'
+puts 'Animal'
+puts 'Object'
+puts 'Kernel'
+puts 'BasicObject'
