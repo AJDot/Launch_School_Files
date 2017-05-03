6b86e4971a61359b7938b5435403cd8504ee8558
Alex Henegar <alex.henegar19@gmail.com>
2017-05-03 03:44:04 +0000

    add card graphic display

--- 120_Object_Oriented_Programming/lesson_5/twenty_one.rb
+++ 120_Object_Oriented_Programming/lesson_5/twenty_one.rb
@@ -1,15 +1,6 @@
-# Twenty-One is a card game consisting of a dealer and a player, where the
-# participants tr tp get as close to 21 as possible without going over.
-# Overview:
-# - Both participants are initially dealt 2 cards from a 52-card deck.
-# - The player takes the first turn, and can "hit" or "stay".
-# - If the player busts, he loses. If he stays, it's the dealer's turn.
-# - The dealer must hit until his cards add up to at least 17.
-# - If he busts, the player wins. If both player and dealer stays, then the
-# highest total wins.
-# - If both totals are equals, then it's a tie, and nobody wins.
-
-require 'pry'
+# If you have the rainbox gem then uncomment lines 15 and 79-82 to add
+# colors to the hearts and diamonds on the cards.
+#require 'rainbow/ext/string'
 
 class Card
   SUITS = %w(C H S D).freeze
@@ -55,8 +46,31 @@ class Card
     face == 'Jack'
   end
 
-  def to_s
-    "The #{face} of #{suit}"
+  def pic_array(hide: false)
+    suit_icon, face_icon = suit_and_face_icons
+    suit_icon = '?' if hide
+    face_icon = '?' if hide
+
+    ['+---------+ ',
+     "| #{face_icon.ljust(3)} #{face_icon.rjust(3)} | ",
+     "| #{suit_icon}     #{suit_icon} | ",
+     '|         | ',
+     "|   #{face_icon.center(3)}   | ",
+     '|         | ',
+     "| #{suit_icon}     #{suit_icon} | ",
+     "| #{face_icon.ljust(3)} #{face_icon.rjust(3)} | ",
+     '+---------+ ']
+  end
+
+  def suit_and_face_icons
+    suit_icon = case @suit
+                when 'S' then "\u2660"#.color(:white)
+                when 'H' then "\u2665"#.color(:red)
+                when 'D' then "\u2666"#.color(:red)
+                when 'C' then "\u2663"#.color(:white)
+                end
+    face_icon = @face
+    [suit_icon, face_icon]
   end
 end
 
@@ -94,10 +108,19 @@ module Hand
 
   def show_hand
     puts "----- #{name}'s Hand -----"
-    cards.each { |card| puts "=> #{card}" }
+    display_cards
     puts ''
     puts "=> Total: #{total}"
     puts ''
+  end
+
+  def display_cards(hide: false)
+    pic_arrays = if hide
+                   [cards.first.pic_array, cards.last.pic_array(hide: true)]
+                 else
+                   cards.map(&:pic_array)
+                 end
+    puts pic_arrays.transpose.map(&:join)
   end
 
   def total
@@ -172,8 +195,7 @@ class Dealer < Participant
 
   def show_flop
     puts "----- #{name}'s Hand -----"
-    puts "=> #{cards.first}"
-    puts '=> ??'
+    puts display_cards(hide: true)
     puts ''
   end
 end
@@ -239,6 +261,7 @@ class Game
 
   def start
     display_welcome_message
+    press_enter_to_continue
     loop do
       clear_and_deal_cards
       show_player_turn_cards
