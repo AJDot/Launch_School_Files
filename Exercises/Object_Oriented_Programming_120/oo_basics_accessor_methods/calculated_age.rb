ca4eef007dbe9dcf7615c871bd9145ada40d9f28
Alex Henegar <alex.henegar19@gmail.com>
2017-05-04 00:45:05 +0000

    add OOP_Inheritance exercises

---
+++ Exercises/Object_Oriented_Programming_120/oo_basics_accessor_methods/calculated_age.rb
@@ -0,0 +1,19 @@
+class Person
+  def age=(new_age)
+    @age = double(new_age)
+  end
+
+  def age
+    double(@age)
+  end
+
+  private
+
+  def double(value)
+    value * 2
+  end
+end
+
+person1 = Person.new
+person1.age = 20
+puts person1.age
