4c50dc8732e8f8ce7e35cc5a6fdf4db71b201173
Alex Henegar <alex.henegar19@gmail.com>
2017-06-25 17:15:52 -0400

    add user_interests code challenge

---
+++ 170_Web_Development/users_interests/users.rb
@@ -0,0 +1,36 @@
+require "yaml"
+
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+before do
+  @users = YAML.load_file "users.yaml"
+end
+
+helpers do
+  def count_interests(users)
+    users.reduce(0) do |sum, (name, user)|
+      sum + user[:interests].size
+    end
+  end
+end
+
+get "/" do
+  redirect "/list"
+end
+
+get "/list" do
+  @title = "Users"
+  erb :list
+end
+
+get "/users/:name" do
+  @name = params[:name].to_sym
+
+  @title = @name.to_s
+  @email = @users[@name][:email]
+  @interests = @users[@name][:interests]
+
+  erb :user
+end
