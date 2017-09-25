1d75e3fb7217efa42aa414caacd86b839fbddcd2
Alex Henegar <alex.henegar19@gmail.com>
2017-09-25 15:36:28 -0400

    add work through lesson 4 - arrays

---
+++ 210_Comp_Thinking_JavaScript/javascript_basics/scrap.js
@@ -0,0 +1,39 @@
+console.log(foo());
+
+function foo() {
+  console.log('Waiting for bar!');
+}
+
+function foo() {
+  console.log(foo);
+  function bar() {
+    console.log('bar again');
+  }
+
+  bar();
+
+  function bar() {
+    console.log('bar again and again');
+  }
+}
+
+// function foo() {
+//   console.log('Waiting for bar!');
+// }
+//
+// function foo() {
+//   function bar() {
+//     console.log('bar again');
+//   }
+//
+//   function bar() {
+//     console.log('bar again and again');
+//   }
+//
+//   console.log(foo);
+//
+//   bar();
+//
+// }
+//
+// console.log(foo());
